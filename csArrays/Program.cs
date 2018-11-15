2df559c086509cd808e095239f1602f6a5a538e6
Dirk Dobson <dirkvancouver@live.com>
2018-11-14 18:27:48 -0700

    inial commit

---
+++ csArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
