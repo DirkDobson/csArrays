ae720d4116c89a14ff213a4d5e4e8da421afe422
Dirk Dobson <dirkvancouver@live.com>
2018-11-14 18:45:27 -0700

    boolean

--- csArrays/Program.cs
+++ csArrays/Program.cs
@@ -7,7 +7,18 @@ namespace csArrays
         static void Main(string[] args)
         {
             var numbers = new int[3];
-            Console.WriteLine(numbers);
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
         }
     }
 }
